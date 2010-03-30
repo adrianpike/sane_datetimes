e6810d92e13ddbd3828f9848bc4c4bbd05319ff4
adrianpike <adrian.pike@gmail.com>
2010-03-29 17:07:42 -0700

    Tweaked the hacks!

--- lib/formtastic_hacks.rb
+++ lib/formtastic_hacks.rb
@@ -1,8 +1,7 @@
 module FormtasticHacks
   def sane_datetime_input(method, options)
 
-    position = { :date => 1, :time => 2 }
-    inputs   = options.delete(:order) || [ :date, :time ]
+    inputs = [ :date, :hour, :separator, :minute, :ampm ]
 
     inputs_capture = ''
     hidden_fields_capture = ''
@@ -11,9 +10,10 @@ module FormtasticHacks
     datetime     = @object ? @object.send(method) : nil
     html_options = (options.delete(:input_html) || {}).with_indifferent_access
 
-    inputs.each do |input|
-      html_id    = generate_html_id(method, "#{position[input]}s")
-      field_name = "#{method}(#{position[input]}s)"
+    inputs.each_index do |index|
+			input = inputs[index]
+      html_id    = generate_html_id(method, "#{index}s")
+      field_name = "#{method}(#{index}s)"
       if options["discard_#{input}".intern]
         break if time_inputs.include?(input)
 
@@ -22,8 +22,22 @@ module FormtasticHacks
       else
         this_html_options = html_options[input] ? html_options.merge(html_options.delete(input)) : html_options
         # item_label_text = I18n.t(input.to_s, :default => input.to_s.humanize, :scope => [:datetime, :prompts])
-
-        inputs_capture << template.text_field_tag("#{@object_name}[#{field_name}]", datetime, this_html_options.merge(:id => html_id))
+				
+				case input
+					when :separator
+						inputs_capture << template.hidden_field_tag("#{@object_name}[#{field_name}]", ':')
+					when :hour
+						hour = (datetime.hour % 12)
+						hour = 12 if hour == 0
+						inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select((1..12).to_a,hour), this_html_options.merge(:id => html_id))
+					when :minute
+						inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select((0..59).to_a.collect{|i| "%02d" % i },datetime.min), this_html_options.merge(:id => html_id))
+					when :ampm
+						am_pm = (datetime.hour >= 12) ? 'PM' : 'AM'
+		       inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select(['AM','PM'],am_pm), this_html_options.merge(:id => html_id))
+					when :date
+						inputs_capture << template.text_field_tag("#{@object_name}[#{field_name}]", datetime.to_date, this_html_options.merge(:id => html_id))
+				end
       end
     end
 
