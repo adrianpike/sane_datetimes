6a2a64d2878bce03608be75eef00f8c2b0756479
adrianpike <adrian.pike@gmail.com>
2010-06-17 18:44:23 -0700

    tweakin the hax

--- lib/formtastic_hacks.rb
+++ lib/formtastic_hacks.rb
@@ -1,7 +1,7 @@
 module FormtasticHacks
   def sane_datetime_input(method, options)
 
-    inputs = [ :date, :hour, :separator, :minute, :ampm ]
+    inputs = [ :date, :time ]
 
     inputs_capture = ''
     hidden_fields_capture = ''
@@ -21,20 +21,29 @@ module FormtasticHacks
         hidden_fields_capture << template.hidden_field_tag("#{@object_name}[#{field_name}]", (hidden_value || 1), :id => html_id)
       else
         this_html_options = html_options[input] ? html_options.merge(html_options.delete(input)) : html_options
-        # item_label_text = I18n.t(input.to_s, :default => input.to_s.humanize, :scope => [:datetime, :prompts])
 				
 				case input
-					when :separator
-						inputs_capture << template.hidden_field_tag("#{@object_name}[#{field_name}]", ':')
-					when :hour
-						hour = (datetime.hour % 12)
-						hour = 12 if hour == 0
-						inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select((1..12).to_a,hour), this_html_options.merge(:id => html_id))
-					when :minute
-						inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select((0..59).to_a.collect{|i| "%02d" % i },datetime.min), this_html_options.merge(:id => html_id))
-					when :ampm
-						am_pm = (datetime.hour >= 12) ? 'PM' : 'AM'
-		       inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select(['AM','PM'],am_pm), this_html_options.merge(:id => html_id))
+					when :time
+						select_options = (0..23).to_a.collect{|i|
+							if i==0
+								['12:00 AM','12:30 AM']
+							else
+								am = i/12.0 < 1
+								if am then
+									["#{"%02d" % (i)}:00 AM", "#{"%02d" % (i)}:30 AM"]
+								else
+									if i == 12
+										["12:00 PM", "12:30 PM"] 
+									else
+										["#{"%02d" % (i-12)}:00 PM", "#{"%02d" % (i-12)}:30 PM"]
+									end
+								end
+							end
+							}.flatten
+						decoded_date = datetime.strftime('%I:%M %p') rescue nil
+
+						inputs_capture << template.select_tag("#{@object_name}[#{field_name}]", template.options_for_select(select_options,decoded_date), this_html_options.merge(:id => html_id))
+
 					when :date
 						inputs_capture << template.text_field_tag("#{@object_name}[#{field_name}]", datetime.to_date, this_html_options.merge(:id => html_id))
 				end
