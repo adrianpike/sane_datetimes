7e826088f9c780ccf9653fb3ab4fc45b79ce907d
adrianpike <adrian.pike@gmail.com>
2010-08-31 14:02:20 -0700

    Fix for Ruby 1.9

--- lib/sane_datetimes.rb
+++ lib/sane_datetimes.rb
@@ -2,7 +2,11 @@ module SaneDatetimes
   def self.included(base)
     define_method :instantiate_time_object_with_two_field_date_times do |name, values|
       begin
-        values = Time.parse(values.join ' ').to_a[0..5].reverse if values.first.is_a? String
+				if RUBY_VERSION >= '1.9'
+					values = Time.strptime(values.join(' '),"%m-%d-%Y %I:%M %p").to_time.to_a[0..5].reverse if values.first.is_a? String
+				else
+					values = Time.parse(values.join ' ').to_a[0..5].reverse if values.first.is_a? String
+				end
         instantiate_time_object_without_two_field_date_times(name, values)
       rescue ArgumentError
         self.errors.add(name, 'bad date')
