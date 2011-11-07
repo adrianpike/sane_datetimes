f947a6680ed64d39ac56e9f624b5b35d62a10c29
Adrian Pike <adrian@pikeapps.com>
2011-11-07 13:57:26 -0800

    bring in the gemification

---
+++ lib/sane_datetime_input.rb
@@ -0,0 +1,72 @@
+class SaneDatetimeInput
+  include Formtastic::Inputs::Base
+  include Formtastic::Inputs::Base::Timeish
+
+  def fragments
+    [:date, :time]
+  end
+  
+  def positions
+    { :date => 1, :time => 2 }
+  end
+  
+  # So close to being able to just reuse core Timeish, unfortunately there's no label_html used in their fragment block
+  def to_html
+    input_wrapping do
+      label_html <<
+      fragments_wrapping do
+        hidden_fragments <<
+        template.content_tag(:ol,
+          fragments.map do |fragment|
+            fragment_wrapping do
+              fragment_label_html(fragment) <<
+              fragment_input_html(fragment)
+            end
+          end.join.html_safe, # TODO is this safe?
+          { :class => 'fragments-group' } # TODO refactor to fragments_group_wrapping
+        )
+      end
+    end
+  end
+  
+  def fragment_label_html(fragment)
+    ''.html_safe
+  end
+  
+  def fragment_input_html(fragment)
+    case fragment
+      when :time
+        select_options = (0..23).to_a.collect{|i|
+          if i==0
+            ['12:00 AM','12:30 AM']
+          else
+            am = i/12.0 < 1
+            if am then
+              ["#{"%02d" % (i)}:00 AM", "#{"%02d" % (i)}:30 AM"]
+            else
+              if i == 12
+                ["12:00 PM", "12:30 PM"] 
+              else
+                ["#{"%02d" % (i-12)}:00 PM", "#{"%02d" % (i-12)}:30 PM"]
+              end
+            end
+          end
+          }.flatten
+        decoded_date = datetime.strftime('%I:%M %p') rescue nil
+
+        template.select_tag(object_name + '[' + fragment_name(fragment) + ']', template.options_for_select(select_options, value ? value.strftime('%I:%M %p') : ''), input_html_options.merge(:id => fragment_id(fragment)))
+      when :date
+        template.text_field_tag(object_name + '[' + fragment_name(fragment) + ']', value ? (I18n.l(value.to_date)) : '', input_html_options.merge(:id => fragment_id(fragment)))
+    end
+  end
+    
+  # TODO: push this back up to Formtastic
+  def fragment_id(fragment)
+    "#{input_html_options[:id]}_#{position(fragment)}s"
+  end
+  
+  def fragment_name(fragment)
+    "#{method}(#{position(fragment)}s)"
+  end
+  
+end
--- lib/sane_datetimes.rb
+++ lib/sane_datetimes.rb
@@ -1,20 +1,22 @@
 module SaneDatetimes
   def self.included(base)
-    define_method :instantiate_time_object_with_two_field_date_times do |name, values|
-      begin
-        if RUBY_VERSION >= '1.9'
-					values = Time.strptime(values.join(' '),"%m/%d/%Y %I:%M %p").to_time.to_a[0..5].reverse if values.first.is_a? String
-				else
-					values = Time.parse(values.join ' ').to_a[0..5].reverse if values.first.is_a? String
-				end
-        instantiate_time_object_without_two_field_date_times(name, values)
-      rescue ArgumentError
-        self.errors.add(name, 'bad date')
-        values = nil
+    
+    define_method :read_time_parameter_value_with_date_string_and_time_string do |name, values_hash_from_param|
+      if values_hash_from_param.keys.size == 2 then
+        # TODO: use the all important locale!
+        parse_format = "%m/%d/%Y %I:%M %p"
+      
+        begin
+          Time.strptime(values_hash_from_param.values.join(' '), parse_format)
+        rescue ArgumentError
+          raise "Invalid Date"
+        end
+      
+      else
+        read_time_parameter_value_without_date_string_and_time_string(name, values_hash_from_param)
       end
     end
-
-    base.alias_method_chain :instantiate_time_object, :two_field_date_times
+    
+    base.alias_method_chain :read_time_parameter_value, :date_string_and_time_string
   end
 end
-
